90cc9ce6424031e8d02d26ed153779d3820cbd2f
ReyChart <reycharton@gmail.com>
2024-04-18 13:23:36 +0300

    Added dropdown menu at user in header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,16 +1,41 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
+import clsx from 'clsx';
 
 import styles from './Header.module.scss';
 
 const Header: FunctionComponent = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className="container">
         <h1 className={styles.header__logo}>Awesome Kanban Board</h1>
-        <div className={styles.header__userimg}>
+        <div className={styles.header__user}>
           <img src="./userAvatar.svg" alt="User avatar" />
-          <IoIosArrowDown />
+          <button
+            onClick={toggleDropdown}
+            className={clsx(styles.btn__arrow, {
+              [styles.arrow__rotate_first]: isDropdownOpen,
+              [styles.arrow__rotate_second]: !isDropdownOpen && isDropdownOpen !== null,
+            })}
+          >
+            <IoIosArrowDown />
+          </button>
+          {isDropdownOpen && (
+            <ul className={styles.header__dropdown}>
+              <li>
+                <a href="#">Profile</a>
+              </li>
+              <li>
+                <a href="#">Log Out</a>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </header>
