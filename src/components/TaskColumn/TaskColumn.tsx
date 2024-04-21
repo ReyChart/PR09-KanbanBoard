99820fb334a04e61b42568250e23baf7da219fde
Rey <126756819+ReyChart@users.noreply.github.com>
2024-04-21 23:26:55 +0300

    Added hooks, styles and fixed bugs in the component

--- src/components/TaskColumn/TaskColumn.tsx
+++ src/components/TaskColumn/TaskColumn.tsx
@@ -1,66 +1,94 @@
-import { ChangeEvent, FunctionComponent, useState } from 'react';
-import Select, { SingleValue } from 'react-select';
+import { FunctionComponent, useState, useMemo, useCallback } from 'react';
 import { ITask } from '../../provider/KanbanContext';
 import { useKanban } from '../../hooks/useKanban';
-import clsx from 'clsx';
 import { FiX } from 'react-icons/fi';
+import Select from 'react-select';
+import clsx from 'clsx';
+
 import styles from './TaskColumn.module.scss';
 
-interface TaskColumnProps {
+interface ITaskColumnProps {
   title: string;
   tasks: ITask[];
   addTaskFunction: (title: string, state: ITask['state']) => void;
   availableTasks?: ITask[];
 }
 
-interface OptionType {
+interface IOptionType {
   value: string;
   label: string;
 }
 
-const TaskColumn: FunctionComponent<TaskColumnProps> = ({
+const TaskColumn: FunctionComponent<ITaskColumnProps> = ({
   title,
   tasks,
   addTaskFunction,
   availableTasks,
 }) => {
-  const [newTaskTitle, setNewTaskTitle] = useState('');
-  const [isAddingTask, setIsAddingTask] = useState(false);
+  const [newTaskTitle, setNewTaskTitle] = useState<string>('');
+  const [addingTask, setAddingTask] = useState<boolean>(false);
   const [hoveredTaskId, setHoveredTaskId] = useState<string | null>(null);
-  const [selectedOption, setSelectedOption] = useState<OptionType | null>(null);
+  const [selectedOption, setSelectedOption] = useState<IOptionType | null>(null);
+  const [attemptedSubmit, setAttemptedSubmit] = useState<boolean>(false);
   const { removeTask, transferTask } = useKanban();
 
-  const handleAddTaskClick = () => {
-    setIsAddingTask(true);
-  };
+  const options = useMemo(
+    () => availableTasks?.map((task) => ({ value: task.id, label: task.title })) || [],
+    [availableTasks]
+  );
 
-  const handleNewTaskTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewTaskTitle(event.target.value);
-  };
+  const { shouldShowSubmit, addButtonDisabled } = useMemo(
+    () => ({
+      shouldShowSubmit: (title === 'Backlog' && newTaskTitle.trim()) || selectedOption,
+      addButtonDisabled:
+        (title !== 'Backlog' && options.length === 0) || (title === 'Backlog' && addingTask),
+    }),
+    [title, newTaskTitle, selectedOption, options.length, addingTask]
+  );
 
-  const handleChange = (option: SingleValue<OptionType>) => {
-    setSelectedOption(option);
-  };
-
-  const handleSubmitNewTask = () => {
-    if (title === 'Backlog' && newTaskTitle.trim() !== '') {
-      addTaskFunction(newTaskTitle.trim(), 'backlog');
-      setNewTaskTitle('');
-      setIsAddingTask(false);
-    } else if (selectedOption) {
-      transferTask(selectedOption.value, title.toLowerCase() as ITask['state']);
-      setSelectedOption(null);
-      setIsAddingTask(false);
+  const handleAddTaskClick = useCallback(() => {
+    if (!addingTask) {
+      setAddingTask(true);
+      setAttemptedSubmit(false);
     }
-  };
+  }, [addingTask]);
 
-  const options: OptionType[] =
-    availableTasks?.map((task) => ({
-      value: task.id,
-      label: task.title,
-    })) || [];
+  const handleNewTaskTitleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTaskTitle(event.target.value);
+  }, []);
 
-  const addButtonDisabled = title !== 'Backlog' && !options.length;
+  const handleSelectChange = useCallback((option: IOptionType | null) => {
+    setSelectedOption(option);
+  }, []);
+
+  const handleSubmitNewTask = useCallback(() => {
+    if (shouldShowSubmit) {
+      const newState = title.toLowerCase() as ITask['state'];
+      selectedOption
+        ? transferTask(selectedOption.value, newState)
+        : addTaskFunction(newTaskTitle.trim(), newState);
+      setNewTaskTitle('');
+      setSelectedOption(null);
+      setAddingTask(false);
+    }
+  }, [shouldShowSubmit, title, newTaskTitle, selectedOption, addTaskFunction, transferTask]);
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setAttemptedSubmit(true);
+      handleSubmitNewTask();
+    },
+    [handleSubmitNewTask]
+  );
+
+  const handleCancel = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    setAddingTask(false);
+    setNewTaskTitle('');
+    setSelectedOption(null);
+    setAttemptedSubmit(false);
+  }, []);
 
   return (
     <div className={styles.task__column}>
@@ -68,7 +96,7 @@ const TaskColumn: FunctionComponent<TaskColumnProps> = ({
       {tasks.map((task) => (
         <div
           key={task.id}
-          className={styles.task}
+          className={styles.task__name}
           onMouseEnter={() => setHoveredTaskId(task.id)}
           onMouseLeave={() => setHoveredTaskId(null)}
         >
@@ -80,45 +108,45 @@ const TaskColumn: FunctionComponent<TaskColumnProps> = ({
           )}
         </div>
       ))}
-      {isAddingTask ? (
-        <div className={styles.task__add}>
-          {title === 'Backlog' ? (
-            <input
-              type="text"
-              value={newTaskTitle}
-              onChange={handleNewTaskTitleChange}
-              placeholder="Enter task title..."
-              className={styles.input}
-            />
-          ) : (
-            <Select
-              className={styles.selector}
-              value={selectedOption}
-              onChange={handleChange}
-              options={options}
-              placeholder="Select a task..."
-            />
-          )}
+      <form className={styles.task__form} onSubmit={handleSubmit}>
+        {addingTask && (
+          <>
+            {title === 'Backlog' ? (
+              <input
+                type="text"
+                value={newTaskTitle}
+                onChange={handleNewTaskTitleChange}
+                placeholder="Enter task title..."
+              />
+            ) : (
+              <Select
+                classNamePrefix="select"
+                value={selectedOption}
+                required={attemptedSubmit && !selectedOption}
+                onChange={handleSelectChange}
+                options={options}
+                placeholder="Select a task..."
+              />
+            )}
+          </>
+        )}
+        <div className={styles.btn__wrapper}>
           <button
-            onClick={handleSubmitNewTask}
-            disabled={!newTaskTitle.trim() && !selectedOption}
-            className={clsx(styles.button, {
-              [styles.btn__add_disabled]: addButtonDisabled,
-              [styles.btn__submit]: newTaskTitle.trim() || selectedOption,
+            onClick={shouldShowSubmit ? handleSubmitNewTask : handleAddTaskClick}
+            disabled={shouldShowSubmit ? false : addButtonDisabled}
+            className={clsx(styles.btn, {
+              [styles.btn__submit]: shouldShowSubmit,
             })}
           >
-            Submit
+            {shouldShowSubmit ? 'Submit' : '+ Add card'}
           </button>
+          {addingTask && (
+            <button onClick={handleCancel} className={clsx(styles.btn, styles.btn__cancel)}>
+              Cancel
+            </button>
+          )}
         </div>
-      ) : (
-        <button
-          onClick={handleAddTaskClick}
-          className={clsx(styles.btn__add, { [styles.btn__add_disabled]: addButtonDisabled })}
-          disabled={addButtonDisabled}
-        >
-          + Add card
-        </button>
-      )}
+      </form>
     </div>
   );
 };
